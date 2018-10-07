eb2b72d4e1c7ef3cc8023077d5b022019b3b5f2e
shafiqu <300621334@cencol.ca>
2018-10-07 10:20:13 -0400

    Add project files.

---
+++ ShafiqWebAppAccess2RDS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShafiqWebAppAccess2RDS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
